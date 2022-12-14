754451ff45f52c57b4d44c5fd03f2b215fcfd250
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 05:00:59 +0500

    Create BABUR cruise missile.py

---
+++ BABUR cruise missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'BABUR cruise missile').hexdigest()
+'61edf4ad3d678ad2c763239e68baa6d0e98b5f3ead48d93639cb45a4a26bb5504c36f7adf39efa78710b4731c1f5e00dc6ab4254ef122261efab25818c2e36e1'
